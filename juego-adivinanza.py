d5ee513c82630228b30e51a5737963c11bfaacf4
Evelyn 💻 <evelynsuarez19@gmail.com>
2025-02-10 10:34:42 -0300

    Update juego-adivinanza.py

---
+++ juego-adivinanza.py
@@ -0,0 +1,30 @@
+# JUEGO DE ADIVINANZA 
+
+import random
+
+numero_secreto = random.randint (0,101)
+numero_adivinado = False
+cantidad_intentos = 0
+cantidad_maxima_intentos = 5
+
+nombre = input ("Ingresa tu nombre: ")
+print ("¡Bienvenidx", nombre, "al juego de adivinar el número secreto!")
+
+
+while not numero_adivinado and cantidad_intentos < cantidad_maxima_intentos:
+    numero_ingresado = int ((input ("Ingresá un número del 0 al 100: ")))
+
+    if numero_ingresado == numero_secreto:
+        print ("¡Felicitaciones", nombre, "adivinaste el número secreto! ¡Ganaste!")
+        numero_adivinado = True
+
+    elif numero_ingresado < numero_secreto:
+        print ("El número secreto es mayor al ingresado")
+
+    else:
+        print ("El número secreto es menor al ingresado")
+    
+    cantidad_intentos += 1
+
+if not cantidad_intentos < cantidad_maxima_intentos:
+    print (nombre, "perdiste el juego. No pudiste adivinar el número secreto dentro de los intentos permitidos.")
